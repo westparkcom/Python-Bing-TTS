737aabc219ce0a2b4879757cc04d9d8d46aa031b
root <root@freeswitch-dev.westpark.local>
2016-11-18 20:41:53 -0600

    Inital commit Library is functional

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Python-Bing-TTS
 Microsoft Bing Text to Speech library for Python
+
+# Usage
+The following is the usage of the library
+
+    translator.speak(text, language, gender, fileformat)
+
+Variable | Description | Note
+--- | --- | ---
+text | The text that you wish to convert to speech | 
+language | The language/country you wish to hear the speech in | Case sensitive. See [Bing TTS API Refernce](https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/api-reference-rest/bingvoiceoutput#4-supported-locales-and-voice-fonts) for list
+gender | Male or Female | Case sensitive
+fileformat | File format to encode the speech to | See [Bing TTS API Reference](https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/api-reference-rest/bingvoiceoutput#http-headers) for list of formats
+
+# Example
+    from bingtts import Translator
+    translator = Translator('YOUR-API-KEY-HERE')
+    output = translator.speak("This is a text to speech translation", "en-US", "Female", "riff-16khz-16bit-mono-pcm")
+    with open("file.wav", "w") as f:
+        f.write(output)
---
+++ bingtts/__init__.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    A library to get text to speech from micrsoft translation engine.
+
+    See: https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/api-reference-rest/bingvoiceoutput
+
+"""
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import requests
+import logging
+
+
+class BadRequestException(Exception):
+    def __init__(self, message):
+        self.message = message.text
+        super(BadRequestException, self).__init__(self.message)
+        
+class AuthException(Exception):
+    def __init__(self, message):
+        self.message = message.text
+        super(AuthException, self).__init__(self.message)
+
+
+class Translator(object):
+    """
+    Implements API for the Microsoft Translator service
+    """
+    auth_url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+    base_url = 'https://speech.platform.bing.com'
+    
+    def __init__(self, client_secret, debug=False):
+        """
+        :param clien_secret: The API key provided by Azure
+        :param debug: If true, the logging level will be set to debug
+        """
+        self.client_secret = client_secret
+        self.debug = debug
+        self.logger = logging.getLogger("bingtts")
+        self.access_token = None
+        if self.debug:
+            self.logger.setLevel(level=logging.DEBUG)
+        
+    def get_access_token(self):
+        """
+        Retrieve access token from Azure.
+        
+        :return: Text of the access token to be used with requests
+        """
+        response = requests.post(
+            self.auth_url,
+            headers={'Ocp-Apim-Subscription-Key' : self.client_secret})
+            
+        if response.status_code != 200:
+            raise AuthException(response)
+        return response.text
+        
+    def call(self, headerfields, path, body):
+        """
+        Calls Bing API and retrieved audio
+        
+        :param headerfields: Dictionary of all headers to be sent
+        :param path: URL path to be appended to requests
+        :param body: Content body to be posted
+        """
+        
+        # If we don't have an access token, get one
+        if not self.access_token:
+            self.access_token = self.get_access_token()
+        
+        # Set authorization header to token we just retrieved
+        headerfields["Authorization"] = "Bearer " + self.access_token
+        # Post to Bing API
+        resp = requests.post(
+            "/".join([self.base_url, path]),
+            headers = headerfields,
+            data = body
+        )
+        # If token was expired, get a new one and try again
+        if resp.status_code == 401:
+            self.access_token = None
+            return self.call(headerfields, path, body)
+        
+        # Bad data or problem, raise exception    
+        if resp.status_code != 200:
+            raise BadRequestException(resp)
+            
+        return resp.text
+        
+    def speak(self, text, lang, gender, format):
+        """
+        Gather parameters and call.
+        
+        :param text: Text to be sent to Bing TTS API to be
+                     converted to speech
+        :param lang: Language to be spoken
+        :param gender: Gender of the speaker
+        :param format: File format (see link below)
+        
+        Name maps and file format specifications can be found here:
+        https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/api-reference-rest/bingvoiceoutput
+        """
+        
+        namemap = {
+            "ar-EG,Female" : "Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)",
+            "de-DE,Female" : "Microsoft Server Speech Text to Speech Voice (de-DE, Hedda)",
+            "de-DE,Male" : "Microsoft Server Speech Text to Speech Voice (de-DE, Stefan, Apollo)",
+            "en-AU,Female" : "Microsoft Server Speech Text to Speech Voice (en-AU, Catherine)",
+            "en-CA,Female" : "Microsoft Server Speech Text to Speech Voice (en-CA, Linda)",
+            "en-GB,Female" : "Microsoft Server Speech Text to Speech Voice (en-GB, Susan, Apollo)",
+            "en-GB,Male" : "Microsoft Server Speech Text to Speech Voice (en-GB, George, Apollo)",
+            "en-IN,Male" : "Microsoft Server Speech Text to Speech Voice (en-IN, Ravi, Apollo)",
+            "en-US,Male" : "Microsoft Server Speech Text to Speech Voice (en-US, BenjaminRUS)",
+            "en-US,Female" : "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)",
+            "es-ES,Female" : "Microsoft Server Speech Text to Speech Voice (es-ES, Laura, Apollo)",
+            "es-ES,Male" : "Microsoft Server Speech Text to Speech Voice (es-ES, Pablo, Apollo)",
+            "es-MX,Male" : "Microsoft Server Speech Text to Speech Voice (es-MX, Raul, Apollo)",
+            "fr-CA,Female" : "Microsoft Server Speech Text to Speech Voice (fr-CA, Caroline)",
+            "fr-FR,Female" : "Microsoft Server Speech Text to Speech Voice (fr-FR, Julie, Apollo)",
+            "fr-FR,Male" : "Microsoft Server Speech Text to Speech Voice (fr-FR, Paul, Apollo)",
+            "it-IT,Male" : "Microsoft Server Speech Text to Speech Voice (it-IT, Cosimo, Apollo)",
+            "ja-JP,Female" : "Microsoft Server Speech Text to Speech Voice (ja-JP, Ayumi, Apollo)",
+            "ja-JP,Male" : "Microsoft Server Speech Text to Speech Voice (ja-JP, Ichiro, Apollo)",
+            "pt-BR,Male" : "Microsoft Server Speech Text to Speech Voice (pt-BR, Daniel, Apollo)",
+            "ru-RU,Female" : "Microsoft Server Speech Text to Speech Voice (ru-RU, Irina, Apollo)",
+            "ru-RU,Male" : "Microsoft Server Speech Text to Speech Voice (ru-RU, Pavel, Apollo)",
+            "zh-CN,Female" : "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)",
+            "zh-CN,Male" : "Microsoft Server Speech Text to Speech Voice (zh-CN, Kangkang, Apollo)",
+            "zh-HK,Male" : "Microsoft Server Speech Text to Speech Voice (zh-HK, Danny, Apollo)",
+            "zh-TW,Female" : "Microsoft Server Speech Text to Speech Voice (zh-TW, Yating, Apollo)",
+            "zh-TW,Male" : "Microsoft Server Speech Text to Speech Voice (zh-TW, Zhiwei, Apollo)"
+            }
+        if not gender:
+            gender = 'Female'
+        if not lang:
+            lang = 'en-US'
+        if not format:
+            format = 'riff-8khz-8bit-mono-mulaw'
+        try:
+            servicename = namemap[lang + ',' + gender]
+        except (Exception) as e:
+            raise BadRequestException(e)
+            
+        headers = {
+            "Content-type" : "application/ssml+xml",
+            "X-Microsoft-OutputFormat" : format,
+            "X-Search-AppId" : "07D3234E49CE426DAA29772419F436CA", 
+            "X-Search-ClientID" : "1ECFAE91408841A480F00935DC390960", 
+            "User-Agent" : "TTSForPython"
+            }
+            
+        body = "<speak version='1.0' xml:lang='%s'><voice xml:lang='%s' xml:gender='%s' name='%s'>%s</voice></speak>" % (lang, lang, gender, servicename, text)
+        
+        return self.call(headers, "synthesize", body)
+        
