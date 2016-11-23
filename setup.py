198587d1c33ea2cf1ce319ef441cfe9753965e64
Westpark Communications <westparkcom@freeswitch-dev.westpark.local>
2016-11-23 10:16:55 -0600

    Added PIP setup capability

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='bingtts',
+    packages=['bingtts'],
+    install_requires=[
+        'json',
+        'httpclient'
+    ],
+    version='0.1.0',
+    description='Python library to access Microsoft Bing Text to Speech API',
+    author='jpattWPC',
+    author_email='jpatten@westparkcom.net',
+    url='https://github.com/westparkcom/Python-Bing-TTS',
+    download_url='https://github.com/westparkcom/Python-Bing-TTS/tarball/master',
+    keywords=['microsoft', 'bing', 'text to speech', 'cognitive'],
+    classifiers=[],
+)
