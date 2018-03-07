66dbb38b7419b2e12c5a6dab80baf9bc40f1a56a
jpattWPC <jpatten@westparkcom.net>
2018-03-07 12:02:55 -0600

    Include sample usage code

--- bingtts.py
+++ bingtts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import hashlib
 import os
 import sys
@@ -8,35 +9,55 @@ import shutil
 
 # Define a few maps for reference
 namemap = {
-    "ar-EG,Female" : "Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)",
-    "de-DE,Female" : "Microsoft Server Speech Text to Speech Voice (de-DE, Hedda)",
-    "de-DE,Male" : "Microsoft Server Speech Text to Speech Voice (de-DE, Stefan, Apollo)",
-    "en-AU,Female" : "Microsoft Server Speech Text to Speech Voice (en-AU, Catherine)",
-    "en-CA,Female" : "Microsoft Server Speech Text to Speech Voice (en-CA, Linda)",
-    "en-GB,Female" : "Microsoft Server Speech Text to Speech Voice (en-GB, Susan, Apollo)",
-    "en-GB,Male" : "Microsoft Server Speech Text to Speech Voice (en-GB, George, Apollo)",
-    "en-IN,Male" : "Microsoft Server Speech Text to Speech Voice (en-IN, Ravi, Apollo)",
-    "en-US,Male" : "Microsoft Server Speech Text to Speech Voice (en-US, BenjaminRUS)",
-    "en-US,Female" : "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)",
-    "es-ES,Female" : "Microsoft Server Speech Text to Speech Voice (es-ES, Laura, Apollo)",
-    "es-ES,Male" : "Microsoft Server Speech Text to Speech Voice (es-ES, Pablo, Apollo)",
-    "es-MX,Male" : "Microsoft Server Speech Text to Speech Voice (es-MX, Raul, Apollo)",
-    "fr-CA,Female" : "Microsoft Server Speech Text to Speech Voice (fr-CA, Caroline)",
-    "fr-FR,Female" : "Microsoft Server Speech Text to Speech Voice (fr-FR, Julie, Apollo)",
-    "fr-FR,Male" : "Microsoft Server Speech Text to Speech Voice (fr-FR, Paul, Apollo)",
-    "he-IL,Male" : "Microsoft Server Speech Text to Speech Voice (he-IL, Asaf)",
-    "it-IT,Male" : "Microsoft Server Speech Text to Speech Voice (it-IT, Cosimo, Apollo)",
-    "ja-JP,Female" : "Microsoft Server Speech Text to Speech Voice (ja-JP, Ayumi, Apollo)",
-    "ja-JP,Male" : "Microsoft Server Speech Text to Speech Voice (ja-JP, Ichiro, Apollo)",
-    "pt-BR,Male" : "Microsoft Server Speech Text to Speech Voice (pt-BR, Daniel, Apollo)",
-    "ru-RU,Female" : "Microsoft Server Speech Text to Speech Voice (ru-RU, Irina, Apollo)",
-    "ru-RU,Male" : "Microsoft Server Speech Text to Speech Voice (ru-RU, Pavel, Apollo)",
-    "zh-CN,Female" : "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)",
-    "zh-CN,Male" : "Microsoft Server Speech Text to Speech Voice (zh-CN, Kangkang, Apollo)",
-    "zh-HK,Male" : "Microsoft Server Speech Text to Speech Voice (zh-HK, Danny, Apollo)",
-    "zh-TW,Female" : "Microsoft Server Speech Text to Speech Voice (zh-TW, Yating, Apollo)",
-    "zh-TW,Male" : "Microsoft Server Speech Text to Speech Voice (zh-TW, Zhiwei, Apollo)"
-    }
+            "ar-EG" : ["Hoda"],
+            "ar-SA" : ["Naayf"],
+            "bg-BG" : ["Hoda"],
+            "ca-ES" : ["HerenaRUS"],
+            "cs-CZ" : ["Jakub"],
+            "da-DK" : ["HelleRUS"],
+            "de-AT" : ["Michael"],
+            "de-CH" : ["Karsten"],
+            "de-DE" : ["Hedda", "HeddaRUS", "Stefan, Apollo"],
+            "el-GR" : ["Stefanos"],
+            "en-AU" : ["Catherine", "HayleyRUS"],
+            "en-CA" : ["Linda", "HeatherRUS"],
+            "en-GB" : ["Susan, Apollo", "HazelRUS", "George, Apollo"],
+            "en-IE" : ["Sean"],
+            "en-IN" : ["Heera, Apollo", "PriyaRUS", "Ravi, Apollo"],
+            "en-US" : ["ZiraRUS", "JessaRUS", "BenjaminRUS"],
+            "es-ES" : ["Laura, Apollo", "HelenaRUS", "Pablo, Apollo"],
+            "es-MX" : ["HildaRUS", "Raul, Apollo"],
+            "fi-FI" : ["HeidiRUS"],
+            "fr-CA" : ["Caroline", "HarmonieRUS"],
+            "fr-CH" : ["Guillaume"],
+            "fr-FR" : ["Julie, Apollo", "HortenseRUS", "Paul, Apollo"],
+            "he-IL" : ["Asaf"],
+            "hi-IN" : ["Kalpana, Apollo", "Kalpana", "Hemant"],
+            "hr-HR" : ["Matej"],
+            "hu-HU" : ["Szabolcs"],
+            "id-ID" : ["Andika"],
+            "it-IT" : ["Cosimo, Apollo"],
+            "ja-JP" : ["Ayumi, Apollo", "Ichiro, Apollo", "HarukaRUS", "LuciaRUS", "EkaterinaRUS"],
+            "ko-KR" : ["HeamiRUS"],
+            "ms-MY" : ["Rizwan"],
+            "nb-NO" : ["HuldaRUS"],
+            "nl-NL" : ["HannaRUS"],
+            "pl-PL" : ["PaulinaRUS"],
+            "pt-BR" : ["HeloisaRUS", "Daniel, Apollo"],
+            "pt-PT" : ["HeliaRUS"],
+            "ro-RO" : ["Andrei"],
+            "ru-RU" : ["Irina, Apollo", "Pavel, Apollo"],
+            "sk-SK" : ["Filip"],
+            "sl-SI" : ["Lado"],
+            "sv-SE" : ["HedvigRUS"],
+            "ta-IN" : ["Valluvar"],
+            "th-TH" : ["Pattara"],
+            "tr-TR" : ["SedaRUS"],
+            "vi-VN" : ["An"],
+            "zh-CN" : ["HuihuiRUS", "Yaoyao, Apollo", "Kangkang, Apollo"],
+            "zh-HK" : ["Tracy, Apollo", "TracyRUS", "Danny, Apollo"],
+            "zh-TW" : ["Yating, Apollo", "HanHanRUS", "Zhiwei, Apollo"]
+}
     
 formatmap = {
     "raw-8khz-8bit-mono-mulaw" : None,
@@ -59,27 +80,28 @@ for item in sys.argv:
 
 try:
     helpReq = argsDict['--help']
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache --dest=/path/to/destination/file.wav --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
-    print "--cache         : Location of file cache. Useful if you repeatedly request"
-    print "                      the same text to speech and wish to conserve bandwidth"
-    print "                      and reduce Azure costs. Must be writeable."
-    print "--dest          : Location for the output file. Must be writeable."
-    print "--lang          : Language and gender to be spoken. Case sensitive."
-    print "--fileformat    : Format of the output file."
-    print "--apikey        : API key generated by Azure."
-    print "--text          : Text to convert to speech. MUST be enclosed in double quotes."
-    print ""
-    print "Available language combinations (--lang):"
-    for item, value in namemap.iteritems():
-        langcombo = item.split(",")
-        print "Language:", langcombo[0], "    Gender:", langcombo[1]
-    print ""
-    print "Available file formats:"
-    for item, value in formatmap.iteritems():
-        print item
-    sys.exit(0)
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
+    print ("--cache         : Location of file cache. Useful if you repeatedly request")
+    print ("                      the same text to speech and wish to conserve bandwidth")
+    print ("                      and reduce Azure costs. Must be writeable.")
+    print ("--dest          : Location for the output file. Must be writeable.")
+    print ("--lang          : Language to be spoken. Case sensitive.")
+    print ("--voice         : Voice to be used. Case sensitive.")
+    print ("--fileformat    : Format of the output file.")
+    print ("--apikey        : API key generated by Azure.")
+    print ("--text          : Text to convert to speech. MUST be enclosed in double quotes.")
+    print ("")
+    print ("Available language combinations (--lang --voice):")
+    for item, value in namemap.items():
+        for voiceval in value:
+            print ("Language: {}   Voice: {}".format(item,voiceval))
+    print ("")
+    print ("Available file formats:")
+    for item, value in formatmap.items():
+        print (item)
+    sys.exit(1)
 except:
     pass
     
@@ -88,51 +110,61 @@ except:
 try:
     destLoc = argsDict['--dest']
 except:
-    print ""
-    print "Error: destination location not specified."
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache --dest=/path/to/destination --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
+    print ("")
+    print ("Error: destination location not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
     sys.exit(1)
 
 try:
-    ttsLangKey = argsDict['--lang']
+    ttsLang = argsDict['--lang']
 except:
-    print ""
-    print "Error: language and gender not specified."
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache  --dest=/path/to/destination --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
+    print ("")
+    print ("Error: language not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
     sys.exit(1)
     
 try:
+    ttsVoice = argsDict['--voice']
+except:
+    print ("")
+    print ("Error: voice not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
+    sys.exit(1)
+
+try:
     ttsFormat = argsDict['--fileformat']
 except:
-    print ""
-    print "Error: file format not specified."
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache --dest=/path/to/destination --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
+    print ("")
+    print ("Error: file format not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
     sys.exit(1)
     
 try:
     ttsText = argsDict['--text']
 except:
-    print ""
-    print "Error: text not specified."
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache --dest=/path/to/destination --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
+    print ("")
+    print ("Error: text not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
     sys.exit(1)
 
 try:
     ttsApiKey = argsDict['--apikey']
 except:
-    print ""
-    print "Error: API key not specified."
-    print ""
-    print "Usage:", sys.argv[0], '--cache=/path/to/cache --dest=/path/to/destination --lang=en-US,Female --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text="Hello World"'
-    print ""
+    print ("")
+    print ("Error: API key not specified.")
+    print ("")
+    print ("Usage: {} --cache=/path/to/cache --dest=/path/to/destination --lang=en-US --voice=\"ZiraUS\" --fileformat=riff-8khz-8bit-mono-mulaw --apikey=YOUR-API-KEY --text=\"Hello World\"".format(sys.argv[0]))
+    print ("")
     sys.exit(1)    
 
 try:
@@ -140,59 +172,55 @@ try:
 except:
     ttsCache = None
 
-try:
-    ttsLangGend = ttsLangKey.split(',')
-    ttsLang = ttsLangGend[0]
-    ttsGender = ttsLangGend[1].title()
-    servicename = namemap[ttsLang + ',' + ttsGender]
-except:
-    print ""
-    print "Error: invalid language or gender specified, or invalid combination: The following language/gender combinations are available:"
-    print ""
-    for item, value in namemap.iteritems():
-        langcombo = item.split(",")
-        print "Language:", langcombo[0], "    Gender:", langcombo[1]
+if ttsVoice not in namemap[ttsLang]:
+    print ("")
+    print ("Error: invalid language or Voice specified, or invalid combination: The following language/Voice combinations are available:")
+    print ("")
+    for item, value in namemap.items():
+        for voiceval in value:
+            print ("Language: {}".format(item))
+            print ("Language: {}   Voice: {}".format(item, voiceval))
     sys.exit(1)
     
 try:
     formatname = formatmap[ttsFormat]
 except:
-    print ""
-    print "Error: invalid format specified: The following formats are available:"
-    print ""
-    for item, value in formatmap.iteritems():
-        print item
+    print ("")
+    print ("Error: invalid format specified: The following formats are available:")
+    print ("")
+    for item, value in formatmap.items():
+        print (item)
     sys.exit(1)
     
 if ttsCache:
     cacheaccess = os.access(ttsCache, os.W_OK)
     if not cacheaccess:
-        print "Cannot write to cache location, ignoring --cache setting..."
+        print ("Cannot write to cache location, ignoring --cache setting...")
         ttsCache = None
 
 m = hashlib.md5()
-# Hash lang+gender+text
-m.update(ttsLang + '-' + ttsGender  + '-' + ttsText)
+# Hash lang+Voice+text
+m.update("{}-{}-{}".format(ttsLang, ttsVoice, ttsText))
 # create filename base on MD5 hash
-filename = str(m.hexdigest()) + ".wav"
+filename = "{}.wav".format(m.hexdigest())
 if ttsCache:
     # If our file already exists, just return it so we don't have to do an API call...
-    if os.path.isfile(ttsCache + '/' + filename):
+    if os.path.isfile(os.path.join(ttsCache, filename)):
         try:
-            shutil.copy2(ttsCache + '/' + filename, destLoc)
+            shutil.copy2(os.path.join(ttsCache, filename), destLoc)
             sys.exit(0)
         except (Exception) as e:
-            print "Could not copy cached file:", e
-            print "Exiting..."
+            print ("Could not copy cached file: {}".format(e))
+            print ("Exiting...")
             sys.exit(1)
 
 # Wait to import this until after we check cache to keep things as speedy as possible...
 from bingtts import Translator
 translator = Translator(ttsApiKey)
 try:
-    data = translator.speak(ttsText, ttsLang, ttsGender, ttsFormat)
+    data = translator.speak(ttsText, ttsLang, ttsVoice, ttsFormat)
 except (Exception) as e:
-    print "Error retrieving speech file:", e
+    print ("Error retrieving speech file: {}".format(e))
     sys.exit(1)
 
 if ttsCache:
@@ -200,21 +228,21 @@ if ttsCache:
         with open(destLoc, 'w') as f:
             f.write(data)
     except (Exception) as e:
-        print "Couldn't write to file", destLoc, ":", e
+        print ("Couldn't write to file {}: {}".format(destLoc, e))
         sys.exit(1)
     try:
-        with open(ttsCache + '/' + filename, 'w') as f:
+        with open(os.path.join(ttsCache, filename), 'w') as f:
             f.write(data)
     except (Exception) as e:
-        cacheFileLoc = ttsCache + '/' + filename
-        print "Couldn't write to file", cacheFileLoc, ":", e
+        cacheFileLoc = os.path.join(ttsCache, filename)
+        print ("Couldn't write to file {}: {}".format(cacheFileLoc, e))
         sys.exit(1)
 else:
     try:
         with open(destLoc, 'w') as f:
             f.write(data)
     except (Exception) as e:
-        print "Couldn't write to file", destLoc, ":", e
+        print ("Couldn't write to file {}: {}".format(destLoc, e))
         sys.exit(1)
         
 sys.exit(0)
